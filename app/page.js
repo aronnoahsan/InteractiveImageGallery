e9a805446165fe596955287d8f4428cfe1cc366a
Aronno Ahsan <aronno.ahsan.sust@gmail.com>
2023-10-31 21:15:54 +0600

    flex galary added

--- app/page.js
+++ app/page.js
@@ -1,6 +1,80 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "@/styles/HomePage.module.css";
 
 export default function Home() {
-  return <main className={styles.main}>Image Gallery</main>;
+  return (
+    <main>
+      <div>
+        <div>N Selected Delete</div>
+      </div>
+      <div className={styles.image__gallery}>
+        <Image
+          src="/images/image-1.webp"
+          alt="Image 1"
+          width={200}
+          height={200}
+        />
+        <Image
+          src="/images/image-2.webp"
+          alt="Image 1"
+          width={200}
+          height={200}
+        />
+        <Image
+          src="/images/image-3.webp"
+          alt="Image 1"
+          width={200}
+          height={200}
+        />
+        <Image
+          src="/images/image-4.webp"
+          alt="Image 1"
+          width={200}
+          height={200}
+        />
+        <Image
+          src="/images/image-5.webp"
+          alt="Image 1"
+          width={200}
+          height={200}
+        />
+        <Image
+          src="/images/image-6.webp"
+          alt="Image 1"
+          width={200}
+          height={200}
+        />
+        <Image
+          src="/images/image-7.webp"
+          alt="Image 1"
+          width={200}
+          height={200}
+        />
+        <Image
+          src="/images/image-8.webp"
+          alt="Image 1"
+          width={200}
+          height={200}
+        />
+        <Image
+          src="/images/image-9.webp"
+          alt="Image 1"
+          width={200}
+          height={200}
+        />
+        <Image
+          src="/images/image-10.jpeg"
+          alt="Image 1"
+          width={200}
+          height={200}
+        />
+        <Image
+          src="/images/image-11.jpeg"
+          alt="Image 11"
+          width={200}
+          height={200}
+        />
+      </div>
+    </main>
+  );
 }
